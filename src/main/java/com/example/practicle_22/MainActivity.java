acf91422acdf3166923c79cf84bca13dfaa20612
vishaldesai22 <155609881+vishaldesai22@users.noreply.github.com>
2024-01-14 19:32:16 +0530

    Add files via upload

---
+++ src/main/java/com/example/practicle_22/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.practicle_22;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.EditText;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView t1 = findViewById(R.id.t1);
+        TextView t2 = findViewById(R.id.t2);
+        TextView t3 = findViewById(R.id.t3);
+
+
+
+
+    }
+}
